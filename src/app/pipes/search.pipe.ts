a7fd1d1d429c0e1a6ddb8369f55c67ef5982affd
Denys Datsiv <dendatsiv@gmail.com>
2023-02-23 14:45:09 +0200

    fix bug with searching bar

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -6,12 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(formData: string[], searchedValue?: string): string[] {
-
     if (!formData) return null;
     if (!searchedValue) return formData;
-
     searchedValue = searchedValue.toLowerCase();
-
     return formData.filter((item: any) => { return JSON.stringify(item).toLocaleLowerCase().includes(searchedValue) })
   }
 
